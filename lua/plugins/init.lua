52b157de2a0ee6f55d225be8e73e824cdea3e987
Alex Tuft <alex.tuft@outlook.com>
2023-06-13 20:36:16 +0100

    Add nvim-treesitter plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,5 +5,14 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim"
         }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            vim.cmd("TSUpdate")
+        end
+    },
+    {
+        "nvim-treesitter/playground"
     }
 }
