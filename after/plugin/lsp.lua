5dca1efb4c100e52bdbdb41d6fc09cee9968dd17
Alex Tuft <alex.tuft@outlook.com>
2023-06-13 22:32:10 +0100

    Setup automatic config for mason-lspconfig

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,7 +12,14 @@ api.nvim_create_autocmd("LspAttach", {
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+require("mason-lspconfig").setup_handlers({
+    function(server_name)
+        require("lspconfig")[server_name].setup({
+            capabilities = capabilities
+        })
+    end
+})
+
 -- lua
 require("lspconfig").lua_ls.setup({
-    capabilities = capabilities
 })
