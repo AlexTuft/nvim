a41441392710e87aa095a728012308c5712c59bf
Alex Tuft <alex.tuft@outlook.com>
2023-06-13 19:37:26 +0100

    Add basic config

---
+++ lua/alex/editor.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt
+
+-- line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.autoindent = true
