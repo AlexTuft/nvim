a9787173b95977f9c51522d7fdf0cc799ee90f2c
Alex Tuft <alex.tuft@outlook.com>
2023-06-16 12:10:23 +0100

    Show filetree in floating window

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,3 +1,6 @@
+local height_ratio = 0.8
+local width_ratio = 0.5
+
 require("nvim-tree").setup({
     disable_netrw = true,
     hijack_netrw = true,
@@ -12,7 +15,29 @@ require("nvim-tree").setup({
         }
     },
     view = {
-        width = 30
+        float = {
+            enable = true,
+            open_win_config = function ()
+                local screen_w = vim.opt.columns:get()
+                local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                local window_w = screen_w * width_ratio
+                local window_h = screen_h * height_ratio
+                local center_x = (screen_w - window_w) / 2
+                local center_y = (vim.opt.lines:get() - window_h) / 2 - vim.opt.cmdheight:get()
+
+                return {
+                    border = "rounded",
+                    relative = "editor",
+                    row = center_y,
+                    col = center_x,
+                    width = math.floor(window_w),
+                    height = math.floor(window_h)
+                }
+            end
+        },
+        width = function()
+            return math.floor(vim.opt.columns:get() * width_ratio)
+        end
     },
     filters = {
         dotfiles = true
