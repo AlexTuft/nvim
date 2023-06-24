da131f31cde574b528212a280253f5d2eb458e11
Alex Tuft <alex.tuft@outlook.com>
2023-06-24 11:16:02 +0100

    Add mappings for vertical motions

--- lua/alex/mappings.lua
+++ lua/alex/mappings.lua
@@ -1,6 +1,16 @@
 vim.g.mapleader = " "
 
+-- File explorer
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
+-- Clipboard Functions
 vim.keymap.set("n", "<leader>p", "\"+p")
 vim.keymap.set("n", "<leader>P", "\"+P")
+
+-- Vertical motions
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Searching
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
