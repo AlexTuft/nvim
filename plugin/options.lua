b23c0d9d23125ac01b10cb975c321a96a9a6853c
Alex Tuft <alex.tuft@outlook.com>
2024-05-13 20:09:18 +0100

    feat: define basic options

---
+++ plugin/options.lua
@@ -0,0 +1,34 @@
+-- Line numbers
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- Indenting
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+
+-- Display whitespace
+vim.opt.list = true
+vim.opt.listchars = {
+    trail = "•",
+}
+
+-- Disable highlight on search
+vim.o.hlsearch = false
+
+-- Sync clipboard between OS and Neovim
+vim.o.clipboard = "unnamedplus"
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- Remove background color
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_command("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
+
+-- Set spellcheck
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
+
