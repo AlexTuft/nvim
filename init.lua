d58430faa501066d80d17dd82fe4762a1ad3962a
Alex Tuft <alex.tuft@outlook.com>
2024-05-13 19:52:20 +0100

    feat: define keymaps

--- init.lua
+++ init.lua
@@ -1,1 +1,1 @@
-print("Hello, world!")
+vim.g.mapleader = " "
---
+++ plugin/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file [e]xplorer" })
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
