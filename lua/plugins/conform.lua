a1c95f6dddb0fa2a8246d48caddaee39ceca8d78
Alex Tuft <alex.tuft@outlook.com>
2024-05-18 16:54:32 +0100

    feat: setup for python

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,7 @@ local conform = require("conform")
 conform.setup({
     formatters_by_ft = {
         lua = { "stylua" },
+        python = { "black" },
     },
     format_on_save = {
         lsp_fallback = true,
--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,12 +1,18 @@
 local lint = require("lint")
 
-lint.linters_by_ft = {}
+lint.linters_by_ft = {
+    python = { "pylint" },
+}
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
     group = lint_augroup,
-    callback = lint.try_lint,
+    callback = function()
+        lint.try_lint()
+    end,
 })
 
-vim.keymap.set({ "n" }, "<leader>l", lint.try_lint, { desc = "Lint" })
+vim.keymap.set({ "n" }, "<leader>l", function()
+    lint.try_lint()
+end, { desc = "Lint" })
