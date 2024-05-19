455c39838fed7ccf0db00bd355920142fd0152ed
Alex Tuft <alex.tuft@outlook.com>
2024-05-19 02:33:48 +0100

    feat: enable inlay hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,6 +16,9 @@ local on_attach = function(_, bufnr)
     nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
     nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
     nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    nmap("<leader>hh", function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = nil }), { bufnr = nil })
+    end, "Toggle inlay [h]ints")
 
     -- See `:help K` for why this keymap
     nmap("K", vim.lsp.buf.hover, "Hover Documentation")
