c881316e2731b6b0b70aedff260b82c5c492ec3f
Alex Tuft <alex.tuft@outlook.com>
2024-05-19 14:08:10 +0100

    feat: add c++ language servers

--- lua/plugins/list/mason.lua
+++ lua/plugins/list/mason.lua
@@ -14,15 +14,24 @@ return {
 
         mason.setup({})
 
+        -- use lspconfig names instead of mason names
+        -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
         mason_lspconfig.setup({
             ensure_installed = {
+                -- c++
+                "cmake",
+                "clangd",
+                -- lua
                 "lua_ls",
+                -- python
                 "pyright",
             },
         })
         mason_tool_installer.setup({
             ensure_installed = {
+                -- lua
                 "stylua",
+                -- python
                 "black",
                 "pylint",
             },
