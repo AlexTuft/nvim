af92a2e0ade04106c828519022c9510350c45729
Alex Tuft <alex.tuft@outlook.com>
2024-06-16 18:29:47 +0100

    show hidden files in oil

--- lua/plugins/list/oil.lua
+++ lua/plugins/list/oil.lua
@@ -2,7 +2,12 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("oil").setup()
+        require("oil").setup({
+            view_options = {
+                -- Show files and directories that start with "."
+                show_hidden = true,
+            },
+        })
 
         vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open file [e]xplorer" })
     end,
