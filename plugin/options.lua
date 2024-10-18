02559bc602d52d50202fb89f877234e3d37e80f9
Alex Tuft <alex.tuft@outlook.com>
2024-10-18 09:52:50 +0100

    change default theme

--- plugin/options.lua
+++ plugin/options.lua
@@ -34,7 +34,7 @@ vim.opt.spell = true
 vim.opt.spelllang = "en_us"
 
 -- theme
-vim.cmd("colorscheme nomai")
+vim.cmd("colorscheme base16-catppuccin-macchiato")
 
 -- highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
